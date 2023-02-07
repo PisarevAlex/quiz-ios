aa0a849c959ccd8ea4c9ebf7bc74102d57d4a0c1
Alexander Pisarev <pisarev@playkot.com>
2023-02-07 13:15:03 +0400

    Commit #2

--- MovieQuiz/Services/StatisticService.swift
+++ MovieQuiz/Services/StatisticService.swift
@@ -22,12 +22,13 @@ final class StatisticServiceImplementation : StatisticService {
     private let userDefaults = UserDefaults.standard
     
     private enum Keys: String {
-        case correct, total, bestGame, gamesCount
+        case correct, correctTotal, bestGame, gamesCount
     }
     
     func store(correct count: Int, total amount: Int) {
 
         gamesCount += 1
+        correctTotal += count
         
         if (!bestGame.isBetterThan(otherResult: count)) {
             bestGame = GameRecord(correct: count, total: amount, date: Date())
@@ -36,18 +37,35 @@ final class StatisticServiceImplementation : StatisticService {
     
     var totalAccuracy: Double {
         get {
-            let correctAnswers = bestGame.correct
-            let totalAnswers = bestGame.total
-            let resultAccuracy = (Double(correctAnswers) * 100 / Double(totalAnswers)).rounded(.toNearestOrEven)
-            return resultAccuracy
+            let correctAnswers = correctTotal
+            let totalAnswers = gamesCount * bestGame.total
+            return (Double(correctAnswers) * 100 / Double(totalAnswers)).rounded(.toNearestOrEven)
+        }
+    }
+    
+    var correctTotal: Int {
+        get {
+            guard let data = userDefaults.data(forKey: Keys.correctTotal.rawValue) else {
+                userDefaults.set(0, forKey: Keys.correctTotal.rawValue)
+                return .init(0)
+            }
+            let optionalString = String(data: data, encoding: .utf8)
+            return Int(optionalString!)!
+        }
+        set {
+            guard let data = try? JSONEncoder().encode(newValue) else {
+                print("Невозможно сохранить результат")
+                return
+            }
+            userDefaults.set(data, forKey: Keys.correctTotal.rawValue)
         }
     }
     
     var gamesCount: Int {
         get {
             guard let data = userDefaults.data(forKey: Keys.gamesCount.rawValue) else {
-                userDefaults.set(1, forKey: Keys.gamesCount.rawValue)
-                return .init(1)
+                userDefaults.set(0, forKey: Keys.gamesCount.rawValue)
+                return .init(0)
             }
             let optionalString = String(data: data, encoding: .utf8)
             return Int(optionalString!)!
